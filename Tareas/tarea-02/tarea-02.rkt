8c6833b1bc2956fb1c853e9a7c3c01d7ac438ae8
Antonio Quihuis <zalcom53@gmail.com>
2022-08-29 15:12:52 -0700

    .
    .

---
+++ Tareas/tarea-02/tarea-02.rkt
@@ -0,0 +1,5 @@
+#tarea-02
+#Nombre: tarea-02.rkt
+#Descripcion: Este archivo corresponde a la tarea 02 de la materia lenguajes de programacion
+#Fecha: semestre 2022-2
+#Autor: Daniel Antonio Quihuis Hernandez
